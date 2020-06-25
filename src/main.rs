65cfef05c1682a9ed9e4623eee42d932bbd3aca3
Rafia10 <parasramsha@gmail.com>
2020-06-25 09:40:48 -0700

    Add  all files

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+// use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+// async fn index() -> impl Responder {
+//     HttpResponse::Ok().body("Hello world welcome to quarter 3!")
+// }
+
+// async fn index2() -> impl Responder {
+//     HttpResponse::Ok().body("Hello world again!")
+// }
+// #[actix_rt::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| {
+//         App::new()
+//             .route("/", web::get().to(index))
+//             .route("/again", web::get().to(index2))
+//     })
+//     .bind("127.0.0.1:8088")?
+//     .run()
+//     .await
+// }
+extern crate actix;
+extern crate futures;
+use futures::{future, Future};
+use actix::*;
+
+// this is our Message
+struct Sum(usize, usize);
+
+// we have to define type of response for `Sum` message
+impl ResponseType for Sum {
+    type Item = usize;
+    type Error = ();
+}
+
+// Actor definition
+struct Addit;
+
+impl Actor for Addit {
+    type Context = Context<Self>;
+}
+
+// now we need to define `MessageHandler` for `Sum` message.
+impl Handler<Sum> for Addit {
+
+    fn handle(&mut self, msg: Sum, ctx: &mut Context<Self>) -> Response<Self, Sum> {
+        Self::reply(msg.0 + msg.1)
+    }
+}
+
+fn main() {
+    let system = System::new("test");
+
+    let addr: Address<_> = Summator.start();
+
+    // Address<A>::call() returns ActorFuture object, so we need to wait for result.
+    // ActorFuture makes sense within Actor execution context, but we can use
+    // Address<A>::call_fut() which return simple Future object.
+    let res = addr.call_fut(Sum(10, 5));
+    
+    system.handle().spawn(res.then(|res| {
+        match res {
+            Ok(Ok(result)) => println!("SUM: {}", result),
+            _ => println!("Something wrong"),
+        }
+        
+        Arbiter::system().send(msgs::SystemExit(0));
+        future::result(Ok(()))
+    }));
+
+    system.run();
+}
